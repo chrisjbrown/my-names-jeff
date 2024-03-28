08d643720e062cc857e8ca8b5409c41249c8d1a0
Christopher Brown <chrisjosbrown@gmail.com>
2024-03-28 17:55:37 +0100

    remove hide show

--- src/scripts/module.ts
+++ src/scripts/module.ts
@@ -7,10 +7,24 @@ import { moduleId } from "./constants";
 // let module: MyModule;
 
 // load templates
-loadTemplates([
-  "../templates/token-config.hbs",
-  "../templates/person-type.hbs",
-]);
+loadTemplates(["../templates/token-config.hbs"]);
+
+const types = [
+  "Human",
+  "Dragon",
+  "Dwarf",
+  "Elf",
+  "Gnome",
+  "Goblin",
+  "Halfling",
+  "Orc",
+  "Tiefling",
+].map((type, index) => {
+  return {
+    name: type,
+    selected: index === 0,
+  };
+});
 
 async function getTableFromPack(name: string) {
   const pack = game.packs.get("person-names");
@@ -34,53 +48,15 @@ Hooks.on("renderTokenConfig", async function (app: any, html: JQuery) {
   const characterTab = html.find(`div[data-tab="character"]`);
   const tokenConfig = await renderTemplate(
     "modules/my-names-jeff/templates/token-config.hbs",
-    { enableTokenRename }
+    { enableTokenRename, types }
   );
 
   characterTab.append(tokenConfig);
-
-  const mnjConfig = characterTab.find("[data-my-names-jeff='token-config']");
-  const enableTokenRenameCheckbox = characterTab.find(
-    "[name='flags.my-names-jeff.enableTokenRename']"
-  );
-
-  const types = [
-    "Human",
-    "Dragon",
-    "Dwarf",
-    "Elf",
-    "Gnome",
-    "Goblin",
-    "Halfling",
-    "Orc",
-    "Tiefling",
-  ].map((type, index) => {
-    return {
-      name: type,
-      selected: index === 0,
-    };
-  });
-
-  const typeSelect = await renderTemplate(
-    "modules/my-names-jeff/templates/person-type.hbs",
-    { types }
-  );
-  mnjConfig.append(typeSelect);
-  const typeSelectEl = mnjConfig.find("[data-my-names-jeff='type-dropdown']");
-  typeSelectEl.hide();
-
-  enableTokenRenameCheckbox.on("click", async (event) => {
-    if (event.target?.checked) {
-      typeSelectEl.show();
-    } else {
-      typeSelectEl.show();
-    }
-  });
 });
 
 Hooks.on("preCreateToken", async function (tokenDocument: any) {
   if (tokenDocument.actor?.flags["my-names-jeff"]?.["enableTokenRename"]) {
-    const type = tokenDocument.actor?.flags["my-names-jeff"]?.["nameType"];
+    const type = tokenDocument.flags["my-names-jeff"]?.["nameType"];
     if (type === "Dragon") {
       const dragonTable = await getTableFromPack(`Dragon Name`);
       let dragonName = await dragonTable.roll();
@@ -100,13 +76,3 @@ Hooks.on("preCreateToken", async function (tokenDocument: any) {
     }
   }
 });
-
-// Hooks.on("renderActorDirectory", (_: Application, html: JQuery) => {
-//   const button = $(
-//     `<button class="cc-sidebar-button" type="button">🐶</button>`
-//   );
-//   button.on("click", () => {
-//     module.dogBrowser.render(true);
-//   });
-//   html.find(".directory-header .action-buttons").append(button);
-// });
