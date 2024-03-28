ce68cc1ec9f14d58fc1854131d69c666db3b9e7b
Christopher Brown <chrisjosbrown@gmail.com>
2024-03-28 17:47:52 +0100

    hide show type dropdown

--- src/scripts/module.ts
+++ src/scripts/module.ts
@@ -7,7 +7,10 @@ import { moduleId } from "./constants";
 // let module: MyModule;
 
 // load templates
-loadTemplates(["../templates/token-config.hbs"]);
+loadTemplates([
+  "../templates/token-config.hbs",
+  "../templates/person-type.hbs",
+]);
 
 async function getTableFromPack(name: string) {
   const pack = game.packs.get("person-names");
@@ -35,6 +38,44 @@ Hooks.on("renderTokenConfig", async function (app: any, html: JQuery) {
   );
 
   characterTab.append(tokenConfig);
+
+  const mnjConfig = characterTab.find("[data-my-names-jeff='token-config']");
+  const enableTokenRenameCheckbox = characterTab.find(
+    "[name='flags.my-names-jeff.enableTokenRename']"
+  );
+
+  const types = [
+    "Human",
+    "Dragon",
+    "Dwarf",
+    "Elf",
+    "Gnome",
+    "Goblin",
+    "Halfling",
+    "Orc",
+    "Tiefling",
+  ].map((type, index) => {
+    return {
+      name: type,
+      selected: index === 0,
+    };
+  });
+
+  const typeSelect = await renderTemplate(
+    "modules/my-names-jeff/templates/person-type.hbs",
+    { types }
+  );
+  mnjConfig.append(typeSelect);
+  const typeSelectEl = mnjConfig.find("[data-my-names-jeff='type-dropdown']");
+  typeSelectEl.hide();
+
+  enableTokenRenameCheckbox.on("click", async (event) => {
+    if (event.target?.checked) {
+      typeSelectEl.show();
+    } else {
+      typeSelectEl.show();
+    }
+  });
 });
 
 Hooks.on("preCreateToken", async function (tokenDocument: any) {
