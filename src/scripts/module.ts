3fe5327f3c0e373083fecf643fe70c4125ba7b08
Christopher Brown <chrisjosbrown@gmail.com>
2024-04-13 09:34:21 +0200

    tokenhud hook

--- src/scripts/module.ts
+++ src/scripts/module.ts
@@ -16,62 +16,93 @@ Hooks.once("init", () => {
   console.log(`Initializing ${moduleId}`);
 });
 
-Hooks.on("renderTokenConfig", async function (app: any, html: JQuery) {
-  if (!game.user.isGM) {
-    return;
-  }
+Hooks.on("renderTokenHUD", (hud: any, html: any) => {
+  // const token = hud.object;
+  html[0].querySelector(`.control-icon[data-action="target"]`)
+    .insertAdjacentHTML("beforebegin", `
+      <div class="control-icon" data-action="token-name">
+          <i class="fas fa-person"></i>
+      </div>
+    `);
 
-  const enableTokenRename: boolean =
-    app.token.getFlag("my-names-jeff", "enableTokenRename") || false;
+  const tokenNameButton = html[0].querySelector(`.control-icon[data-action="token-name"]`);
 
-  const selectedType =
-    app.token.flags["my-names-jeff"]?.["nameType"] || "Human";
-  const types = tokenTypes.map((type) => {
-    return {
-      name: type,
-      selected: type === selectedType,
-    };
+  tokenNameButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    console.log('token name click');
+
+    // const button = event.currentTarget;
+
+    // button.classList.toggle("active");
+    // button.querySelector(`.my-names-jeff.token-names`).classList.toggle("active");
   });
 
-  const characterTab = html.find(`div[data-tab="character"]`);
-  const tokenConfig = await renderTemplate(
-    "modules/my-names-jeff/templates/token-config.hbs",
-    { enableTokenRename, types }
-  );
-
-  characterTab.append(tokenConfig);
+  // tokenNameButton.insertAdjacentHTML("beforeend", `
+  //     <div class="my-names-jeff token-names">
+  //         ${visionModes.map(mode => `
+  //         <div class="vision-5e token-name ${mode.id === token.document.sight.visionMode ? "active" : ""} flexrow" data-token-name="${mode.id}">
+  //             <span class="vision-5e token-name-label">${game.i18n.localize(mode.label)}</span>
+  //         </div>`).join("")}
+  //     </div>
+  // `);
 });
 
-Hooks.on("createToken", async function (actor: any) {
-  if (actor?.flags["my-names-jeff"]?.["enableTokenRename"]) {
-    const type = actor.flags["my-names-jeff"]?.["nameType"];
-    if (type === "Dragon") {
-      try {
-        const dragonNameTable = await getTableFromPack(`Dragon Name`);
-        const dragonName = await dragonNameTable.roll();
-        actor.update({ name: `${dragonName.results[0].text}` });
-      } catch (error) {
-        console.error("my-names-jeff", "Error getting Dragon name");
-      }
-      return;
-    } else {
-      try {
-        const [firstNameTable, lastNameTable] = await Promise.all([
-          getTableFromPack(`${type} First Name`),
-          getTableFromPack(`${type} Last Name`),
-        ]);
-        const [firstName, lastName] = await Promise.all([
-          firstNameTable.roll(),
-          lastNameTable.roll(),
-        ]);
+// Hooks.on("renderTokenConfig", async function (app: any, html: JQuery) {
+//   if (!game.user.isGM) {
+//     return;
+//   }
 
-        actor.update({
-          name: `${firstName.results[0].text} ${lastName.results[0].text}`,
-        });
-      } catch (error) {
-        console.error("my-names-jeff", `Error getting ${type} name`);
-      }
-      return;
-    }
-  }
-});
+//   const enableTokenRename: boolean =
+//     app.token.getFlag("my-names-jeff", "enableTokenRename") || false;
+
+//   const selectedType =
+//     app.token.flags["my-names-jeff"]?.["nameType"] || "Human";
+//   const types = tokenTypes.map((type) => {
+//     return {
+//       name: type,
+//       selected: type === selectedType,
+//     };
+//   });
+
+//   const characterTab = html.find(`div[data-tab="character"]`);
+//   const tokenConfig = await renderTemplate(
+//     "modules/my-names-jeff/templates/token-config.hbs",
+//     { enableTokenRename, types }
+//   );
+
+//   characterTab.append(tokenConfig);
+// });
+
+// Hooks.on("createToken", async function (actor: any) {
+//   if (actor?.flags["my-names-jeff"]?.["enableTokenRename"]) {
+//     const type = actor.flags["my-names-jeff"]?.["nameType"];
+//     if (type === "Dragon") {
+//       try {
+//         const dragonNameTable = await getTableFromPack(`Dragon Name`);
+//         const dragonName = await dragonNameTable.roll();
+//         actor.update({ name: `${dragonName.results[0].text}` });
+//       } catch (error) {
+//         console.error("my-names-jeff", "Error getting Dragon name");
+//       }
+//       return;
+//     } else {
+//       try {
+//         const [firstNameTable, lastNameTable] = await Promise.all([
+//           getTableFromPack(`${type} First Name`),
+//           getTableFromPack(`${type} Last Name`),
+//         ]);
+//         const [firstName, lastName] = await Promise.all([
+//           firstNameTable.roll(),
+//           lastNameTable.roll(),
+//         ]);
+
+//         actor.update({
+//           name: `${firstName.results[0].text} ${lastName.results[0].text}`,
+//         });
+//       } catch (error) {
+//         console.error("my-names-jeff", `Error getting ${type} name`);
+//       }
+//       return;
+//     }
+//   }
+// });
